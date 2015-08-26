8429a075fea01000720a355d71b97482014e164f
z1986s8x11 <z63607793>
2015-08-26 15:50:31 +0800

    fix bugs

--- app/src/main/java/com/zsx/app/Lib_BaseActivity.java
+++ app/src/main/java/com/zsx/app/Lib_BaseActivity.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
-import android.view.Gravity;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -161,6 +159,8 @@ public class Lib_BaseActivity extends Activity {
         Lib_SystemExitManager.exitSystem();
     }
 
+    private String mToastMessage = "再次点击退出";
+
     /**
      * 设置 连续双击后退键 退出
      *
@@ -168,6 +168,12 @@ public class Lib_BaseActivity extends Activity {
      */
     public final void _setDoubleBackExit(boolean isDoubleBack) {
         this.isDoubleBack = isDoubleBack;
+    }
+
+
+    public final void _setDoubleBackExit(boolean isDoubleBack, String toastMessage) {
+        this.isDoubleBack = isDoubleBack;
+        this.mToastMessage = toastMessage;
     }
 
     /**
@@ -179,24 +185,35 @@ public class Lib_BaseActivity extends Activity {
     }
 
     @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
+    public void onBackPressed() {
         if (!isDoubleBack) {
-            return super.onKeyDown(keyCode, event);
+            super.onBackPressed();
+            return;
         }
-        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-            if ((System.currentTimeMillis() - exitTime) > 2000) {
-                Toast toast = Toast
-                        .makeText(this, "再次点击退出", Toast.LENGTH_SHORT);
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
-                exitTime = System.currentTimeMillis();
-            } else {
-                _exitSystem();
-            }
-            return true;
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            _showToast(mToastMessage);
+            exitTime = System.currentTimeMillis();
+        } else {
+            _exitSystem();
         }
-        return super.onKeyDown(keyCode, event);
     }
+
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (!isDoubleBack) {
+//            return super.onKeyDown(keyCode, event);
+//        }
+//        if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
+//            if ((System.currentTimeMillis() - exitTime) > 2000) {
+//                _showToast(mToastMessage);
+//                exitTime = System.currentTimeMillis();
+//            } else {
+//                _exitSystem();
+//            }
+//            return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
 
 
 }
