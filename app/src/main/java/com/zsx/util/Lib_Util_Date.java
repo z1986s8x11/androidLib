d45f9ff9ba8494bd9e7b54f0b464b868e835408d
z1986s8x11 <z1986s8x11@163.com>
2015-12-09 09:19:37 +0800

    init

--- app/src/main/java/com/zsx/util/Lib_Util_Date.java
+++ app/src/main/java/com/zsx/util/Lib_Util_Date.java
@@ -7,8 +7,12 @@ import java.util.Date;
 
 public class Lib_Util_Date {
 	/**
-	 * 字符串转化为时间 yyyy-MM-dd HHmmss
-	 * */
+	 * 字符串转化成日期 </br> y 年 M 月 d 日 H 小时 m 分钟
+	 *
+	 * @param dateStr   如 2011-11-11
+	 * @param dateFormat 如 yyyy-MM-dd
+	 * @return 日期
+	 */
 	@SuppressLint("SimpleDateFormat")
 	public static final Date toDate(String dateStr, String dateFormat) {
 		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
--- app/src/main/java/com/zsx/util/Lib_Util_Double.java
+++ app/src/main/java/com/zsx/util/Lib_Util_Double.java
@@ -2,14 +2,24 @@ package com.zsx.util;
 
 import android.text.TextUtils;
 
+import java.math.BigDecimal;
+
 /**
  * Created by Administrator on 2015/12/8.
  */
 public class Lib_Util_Double {
+
     public static double parseDouble(String doubleString) {
         if (TextUtils.isEmpty(doubleString)) {
             return 0d;
         }
         return Double.parseDouble(doubleString);
+    }
+
+    /**
+     * 转化为2位小数
+     */
+    public static double to2Decimals(double decimals) {
+        return new BigDecimal(decimals).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
     }
 }
