0a4cfc66de63c2476719977347d6cd69768fd2ee
z1986s8x11 <z1986s8x11@163.com>
2015-12-24 11:24:52 +0800

    init

--- app/src/main/java/com/zsx/widget/view/Lib_Widget_TextView.java
+++ app/src/main/java/com/zsx/widget/view/Lib_Widget_TextView.java
@@ -9,6 +9,35 @@ import android.widget.TextView;
 import com.zsx.tools.Lib_ShapeHelper;
 
 /**
+ * <attr name="bottomLeftRadius" format="dimension" />
+ * <attr name="bottomRightRadius" format="dimension" />
+ * <attr name="radius" format="dimension" />
+ * <attr name="topLeftRadius" format="dimension" />
+ * <attr name="topRightRadius" format="dimension" />
+ * <attr name="solidColor" format="color" />
+ * <attr name="solidColor2" format="color" />
+ * <attr name="strokeColor" format="color" />
+ * <attr name="strokeColor2" format="color" />
+ * <attr name="strokeDashWidth" format="dimension" />
+ * <attr name="strokeDashGap" format="dimension" />
+ * <attr name="strokeWidth" format="dimension" />
+ * <attr name="gradientStartColor" format="color" />
+ * <attr name="gradientCenterColor" format="color" />
+ * <attr name="gradientEndColor" format="color" />
+ * <attr name="status">
+ * <enum name="pressed" value="0" />
+ * <enum name="enabled" value="1" />
+ * <enum name="checked" value="2" />
+ * <enum name="selected" value="3" />
+ * </attr>
+ * <attr name="textColorStatus">
+ * <enum name="pressed" value="0" />
+ * <enum name="enabled" value="1" />
+ * <enum name="checked" value="2" />
+ * <enum name="selected" value="3" />
+ * </attr>
+ * <attr name="textColor2" format="color" />
+ * <p>
  * Created by Administrator on 2015/12/22.
  */
 public class Lib_Widget_TextView extends TextView {
