ca2cc5da1a5cdd4a76a82fa76a133216be683338
z1986s8x11 <z1986s8x11@163.com>
2016-01-25 18:42:02 +0800

    init

--- LibTest/src/main/java/zsx/com/test/ui/test/Entity.java
+++ LibTest/src/main/java/zsx/com/test/ui/test/Entity.java
@@ -1,15 +1,15 @@
 package zsx.com.test.ui.test;
 
 
-//import com.zsx.apt.database.Lib_SQL;
-//import com.zsx.apt.database.SQLFieldEnum;
-//
-///**
-// * Author       zhusx
-// * Email        327270607@qq.com
-// * Created      2016/1/25.15:33
-// */
-//public class Entity {
-//    @Lib_SQL(_type = SQLFieldEnum.String)
-//    public String a;
-//}
+import com.zsx.apt.database.Lib_SQL;
+import com.zsx.apt.database.SQLFieldEnum;
+
+/**
+ * Author       zhusx
+ * Email        327270607@qq.com
+ * Created      2016/1/25.15:33
+ */
+public class Entity {
+    @Lib_SQL(_type = SQLFieldEnum.String)
+    public String a;
+}
