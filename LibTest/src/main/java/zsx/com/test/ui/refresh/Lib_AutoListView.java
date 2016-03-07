dd0651ca4fc738c2623dc545dfaa16266c6c08f2
z1986s8x11 <z1986s8x11@163.com>
2016-03-07 11:11:50 +0800

    init

--- LibTest/src/main/java/zsx/com/test/ui/refresh/Lib_AutoListView.java
+++ LibTest/src/main/java/zsx/com/test/ui/refresh/Lib_AutoListView.java
@@ -1,20 +1,22 @@
 package zsx.com.test.ui.refresh;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.AttributeSet;
-import android.view.MotionEvent;
-import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 import android.widget.ListView;
 
-import com.zsx.debug.LogUtil;
-import com.zsx.util.Lib_Util_Widget;
-
 /**
- * Created by Administrator on 2016/1/13.
+ * Author       zhusx
+ * Email        327270607@qq.com
+ * Created      2016/3/7 10:13
  */
-public abstract class Lib_AutoListView extends ListView {
-    private View headView;
+public class Lib_AutoListView extends ListView {
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public Lib_AutoListView(Context context) {
         super(context);
@@ -26,231 +28,60 @@ public abstract class Lib_AutoListView extends ListView {
         init();
     }
 
-    protected void init() {
-        setBackgroundDrawable(null);
+    public Lib_AutoListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public Lib_AutoListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
         setCacheColorHint(Color.TRANSPARENT);
         setHorizontalFadingEdgeEnabled(false);
         setVerticalFadingEdgeEnabled(false);
-        headView = initHeadView();
-        Lib_Util_Widget.measureView(headView);
-        headContentHeight = headView.getMeasuredHeight();
-        headView.setPadding(0, -headContentHeight, 0, 0);
     }
 
-    protected abstract View initHeadView();
-
-    protected enum HeadStatus {
-        RELEASE_To_REFRESH, // 松开刷新
-        PULL_To_REFRESH, // 下拉刷新
-        REFRESHING, // 正在刷新
-        DONE // 默认
-    }
-
-    protected enum FootStatus {
-        DONE, // 默认
-        LOADING, // 正在加载更多
-        NO_DATA, // 没有数据
-        ERROR// 加载数据失败
-    }
-
-    private HeadStatus headStatus = HeadStatus.DONE;
-    private FootStatus footStatus = null;
-    // 实际的padding的距离与界面上偏移距离的比例
-    private final static int RATIO = 3;
-    // 用于保证startY的值在一个完整的touch事件中只被记录一次
-    private boolean isRecored;
-    protected int headContentHeight;
-    private int startY;
-    /**
-     * 控制滑动下拉惯性 启动下拉刷新
-     */
-    private boolean isUserTouch;
-    private boolean isBack;
-
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                isUserTouch = true;
-                if (getFirstVisiblePosition() == 0 && !isRecored) {
-                    isRecored = true;
-                    startY = (int) event.getY();
-                    // Log.v(TAG, "在down时候记录当前位置‘");
+    public void _setOnRefreshListener(OnRefreshListener listener) {
+        if (swipeRefreshLayout == null) {
+            ViewParent parent = getParent();
+            if (parent == null) {
+                throw new NullPointerException(" 必须有父容器");
+            }
+            if (!(parent instanceof ViewGroup)) {
+                throw new IllegalArgumentException("parent must is ViewGroup");
+            }
+            ViewGroup viewGroup = (ViewGroup) parent;
+            int index = 0;
+            for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                if (viewGroup.getChildAt(i) == this) {
+                    index = i;
+                    break;
                 }
-                break;
-
-            case MotionEvent.ACTION_UP:
-                if (footStatus != FootStatus.LOADING
-                        && headStatus != HeadStatus.REFRESHING) {
-                    if (headStatus == null || headStatus == HeadStatus.DONE) {
-//					if (event.getY() - startY < 40) {
-//					}
-                    }
-                    if (headStatus == HeadStatus.DONE) {
-                        // 什么都不做
-                    }
-                    if (headStatus == HeadStatus.PULL_To_REFRESH) {
-                        changeHeaderViewByState(HeadStatus.DONE);
-                        // Log.v(TAG, "由下拉刷新状态，到done状态");
-                    }
-                    if (headStatus == HeadStatus.RELEASE_To_REFRESH) {
-                        changeHeaderViewByState(HeadStatus.REFRESHING);
-                        // Log.v(TAG, "由松开刷新状态，到done状态");
-                    }
-                }
-                isUserTouch = false;
-                isRecored = false;
-                isBack = false;
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-                if (isUserTouch) {
-                    int tempY = (int) event.getY();
-
-                    if (!isRecored && getFirstVisiblePosition() == 0) {
-                        // Log.v(TAG, "在move时候记录下位置");
-                        isRecored = true;
-                        startY = tempY;
-                    }
-                    if (headStatus != HeadStatus.REFRESHING && isRecored
-                            && footStatus != FootStatus.LOADING) {
-
-                        // 保证在设置padding的过程中，当前的位置一直是在head，否则如果当列表超出屏幕的话，当在上推的时候，列表会同时进行滚动
-
-                        // 可以松手去刷新了
-                        if (headStatus == HeadStatus.RELEASE_To_REFRESH) {
-                            setSelection(0);
-                            // 往上推了，推到了屏幕足够掩盖head的程度，但是还没有推到全部掩盖的地步
-                            if (((tempY - startY) / RATIO < headContentHeight)
-                                    && (tempY - startY) > 0) {
-                                changeHeaderViewByState(HeadStatus.PULL_To_REFRESH);
-                                // Log.v(TAG, "由松开刷新状态转变到下拉刷新状态");
-                            }
-                            // 一下子推到顶了
-                            else if (tempY - startY <= 0) {
-                                changeHeaderViewByState(HeadStatus.DONE);
-                                // Log.v(TAG, "由松开刷新状态转变到done状态");
-                            }
-                            // 往下拉了，或者还没有上推到屏幕顶部掩盖head的地步
-                            else {
-                                // 不用进行特别的操作，只用更新paddingTop的值就行了
-                            }
-                        }
-                        // 还没有到达显示松开刷新的时候,DONE或者是PULL_To_REFRESH状态
-                        if (headStatus == HeadStatus.PULL_To_REFRESH) {
-                            setSelection(0);
-                            // 下拉到可以进入RELEASE_TO_REFRESH的状态
-                            if ((tempY - startY) / RATIO >= headContentHeight) {
-                                isBack = true;
-                                changeHeaderViewByState(HeadStatus.RELEASE_To_REFRESH);
-                                // Log.v(TAG, "由done或者下拉刷新状态转变到松开刷新");
-                            }
-                            // 上推到顶了
-                            else if (tempY - startY <= 0) {
-                                changeHeaderViewByState(HeadStatus.DONE);
-                                // Log.v(TAG, "由DOne或者下拉刷新状态转变到done状态");
-                            }
-                        }
-
-                        // done状态下
-                        if (headStatus == HeadStatus.DONE) {
-                            if (tempY - startY > 0) {
-                                changeHeaderViewByState(HeadStatus.PULL_To_REFRESH);
-                            }
-                        }
-
-                        // 更新headView的size
-                        if (headStatus == HeadStatus.PULL_To_REFRESH) {
-                            int offset = -1 * headContentHeight + (tempY - startY) / RATIO;
-                            changeHeaderViewByState(HeadStatus.PULL_To_REFRESH, offset);
-                        }
-
-                        // 更新headView的paddingTop
-                        if (headStatus == HeadStatus.RELEASE_To_REFRESH) {
-                            int offset = (tempY - startY) / RATIO - headContentHeight;
-                            changeHeaderViewByState(HeadStatus.RELEASE_To_REFRESH, offset);
-                        }
-                    }
-                }
-                break;
-            case MotionEvent.ACTION_CANCEL:
-                if (headStatus != HeadStatus.REFRESHING && isRecored
-                        && footStatus != FootStatus.LOADING) {
-                    changeHeaderViewByState(HeadStatus.DONE);
-                }
-                break;
+            }
+            viewGroup.removeView(this);
+            ViewGroup.LayoutParams lp = getLayoutParams();
+            swipeRefreshLayout = new SwipeRefreshLayout(getContext());
+            swipeRefreshLayout.addView(this, new SwipeRefreshLayout.LayoutParams(SwipeRefreshLayout.LayoutParams.MATCH_PARENT, SwipeRefreshLayout.LayoutParams.MATCH_PARENT));
+            viewGroup.addView(swipeRefreshLayout, index, lp);
         }
-        return super.onTouchEvent(event);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+            }
+        });
     }
 
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        int action = ev.getAction() & ev.getActionMasked();
-        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_DOWN) {
-            return true;
-        }
-        switch (action) {
-//            case MotionEvent.ACTION_CANCEL:
-//                LogUtil.e("dispatchTouchEvent", "ACTION_CANCEL");
-//                return true;
-//            case MotionEvent.ACTION_UP:
-//                LogUtil.e("dispatchTouchEvent", "ACTION_UP");
-//                return true;
-            case MotionEvent.ACTION_MOVE:
-                LogUtil.e("dispatchTouchEvent", "ACTION_MOVE");
-                if (HeadStatus.DONE != headStatus) {
-                    return false;
-                }
-                break;
-//            case MotionEvent.ACTION_DOWN:
-//                LogUtil.e("dispatchTouchEvent", "ACTION_DOWN");
-//                return true;
-
-        }
-        return super.dispatchTouchEvent(ev);
-    }
-
-    private void changeHeaderViewByState(HeadStatus state) {
-        this.headStatus = state;
-        switch (state) {
-            case RELEASE_To_REFRESH:
-                moveToRefresh(headView);
-                // Log.v(TAG, "当前状态，松开刷新");
-                break;
-            case PULL_To_REFRESH:
-                // 是由RELEASE_To_REFRESH状态转变来的
-                moveToReversal(headView, isBack);
-                isBack = false;
-                // Log.v(TAG, "当前状态，下拉刷新");
-                break;
-
-            case REFRESHING:
-                upToRefresh(headView);
-                // Log.v(TAG, "当前状态,正在刷新...");
-                break;
-            case DONE:
-                defaultStatus(headView);
-                // Log.v(TAG, "当前状态，done");
-                break;
+    public void _setRefresh(boolean refreshing) {
+        if (swipeRefreshLayout != null) {
+            swipeRefreshLayout.setRefreshing(refreshing);
         }
     }
 
-    protected abstract void moveToReversal(View headView, boolean isBack);
-
-    protected abstract void moveToRefresh(View headView);
-
-    protected abstract void upToRefresh(View headView);
-
-    protected abstract void defaultStatus(View headView);
-
-    private void changeHeaderViewByState(HeadStatus state, int offset) {
-        switch (state) {
-            case RELEASE_To_REFRESH:
-                scrollToRefresh(headView, offset, true);
-                break;
-            case PULL_To_REFRESH:
-                scrollToRefresh(headView, offset, false);
-                break;
-        }
+    public interface OnRefreshListener extends SwipeRefreshLayout.OnRefreshListener {
     }
-
-    protected abstract void scrollToRefresh(View headView, int offset, boolean isTop);
 }
--- LibTest/src/main/java/zsx/com/test/ui/refresh/RefreshLinearLayoutActivity.java
+++ LibTest/src/main/java/zsx/com/test/ui/refresh/RefreshLinearLayoutActivity.java
@@ -2,6 +2,8 @@ package zsx.com.test.ui.refresh;
 
 import android.os.Bundle;
 
+import com.zsx.debug.LogUtil;
+
 import zsx.com.test.R;
 import zsx.com.test.base._BaseActivity;
 
@@ -14,5 +16,18 @@ public class RefreshLinearLayoutActivity extends _BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_refreshlayout);
+        final Lib_AutoListView autoList = (Lib_AutoListView) findViewById(R.id.listView);
+        autoList._setOnRefreshListener(new Lib_AutoListView.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                LogUtil.e(this, "===========");
+                autoList.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        autoList._setRefresh(false);
+                    }
+                }, 5000);
+            }
+        });
     }
 }
