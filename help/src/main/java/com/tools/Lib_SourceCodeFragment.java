e868a0045a234fde7d614d015aea9326b5cc5b7c
z1986s8x11 <z1986s8x11@163.com>
2016-03-16 17:43:59 +0800

    init

---
+++ help/src/main/java/com/tools/Lib_SourceCodeFragment.java
@@ -0,0 +1,28 @@
+package com.tools;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.zsx.app.Lib_BaseFragment;
+
+/**
+ * Author       zhusx
+ * Email        327270607@qq.com
+ * Created      2016/3/16 17:28
+ */
+public class Lib_SourceCodeFragment extends Lib_BaseFragment {
+    private WebView mWebView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mWebView = new WebView(inflater.getContext());
+        mWebView.getSettings().setSupportZoom(true);
+        mWebView.getSettings().setBuiltInZoomControls(true);
+        return mWebView;
+    }
+}
