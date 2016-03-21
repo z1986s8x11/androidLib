51fd888a10e7f26bb81c7e529cf9758cd462c76b
z1986s8x11 <z1986s8x11@163.com>
2016-03-21 17:48:37 +0800

    init

--- help/src/main/java/org/zsx/android/api/widget/TabHost_Activity.java
+++ help/src/main/java/org/zsx/android/api/widget/TabHost_Activity.java
@@ -15,47 +15,42 @@ import org.zsx.android.api.R;
 @SuppressWarnings("deprecation")
 public class TabHost_Activity extends TabActivity {
 
-	private Lib_Class_ShowCodeUtil showCode;
+    private Lib_Class_ShowCodeUtil showCode;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		showCode = new Lib_Class_ShowCodeUtil();
-		showCodeInit(showCode);
-		TabHost host = getTabHost();
-		/**
-		 * 设置使用TabHost布局 **
-		 */
-		LayoutInflater.from(this).inflate(R.layout.widget_tabhost,
-				host.getTabContentView(), true);
-		host.addTab(host.newTabSpec("第一页").setIndicator("已接电话")
-				.setContent(R.id.global_linearlayout1));
-		host.addTab(host
-				.newTabSpec("第二页")
-				.setIndicator(
-						"魔兽世界",
-						getResources().getDrawable(
-								android.R.drawable.ic_menu_add))
-				.setContent(R.id.global_linearlayout2));
-		// tabHost 还可以装载另一个activity
-		host.addTab(host.newTabSpec("判断用").setIndicator("显示用")
-				.setContent(new Intent(this, TextView_Activity.class)));
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showCode = new Lib_Class_ShowCodeUtil();
+        showCode.setShowJava(this.getClass());
+        TabHost host = getTabHost();
+        /**
+         * 设置使用TabHost布局 **
+         */
+        LayoutInflater.from(this).inflate(R.layout.widget_tabhost,
+                host.getTabContentView(), true);
+        host.addTab(host.newTabSpec("第一页").setIndicator("已接电话")
+                .setContent(R.id.global_linearlayout1));
+        host.addTab(host
+                .newTabSpec("第二页")
+                .setIndicator(
+                        "魔兽世界",
+                        getResources().getDrawable(
+                                android.R.drawable.ic_menu_add))
+                .setContent(R.id.global_linearlayout2));
+        // tabHost 还可以装载另一个activity
+        host.addTab(host.newTabSpec("判断用").setIndicator("显示用")
+                .setContent(new Intent(this, TextView_Activity.class)));
+    }
 
-	/****************** 显示代码相关 ***************************/
-	public void showCodeInit(Lib_Class_ShowCodeUtil showCode) {
-		showCode.setShowJava(this.getClass());
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        showCode._onCreateOptionsMenu(this, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		showCode._onCreateOptionsMenu(this, menu);
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		showCode._onOptionsItemSelected(this, item);
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        showCode._onOptionsItemSelected(this, item);
+        return super.onOptionsItemSelected(item);
+    }
 }
