6f1587fc5394978432ce70e625e289c727fc42e8
z1986s8x11 <z1986s8x11@163.com>
2016-03-22 11:03:19 +0800

    init

--- LibTest/src/main/java/zsx/com/test/base/_BaseActivity.java
+++ LibTest/src/main/java/zsx/com/test/base/_BaseActivity.java
@@ -1,8 +1,11 @@
 package zsx.com.test.base;
 
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.zsx.app.Lib_BaseActivity;
+import com.zsx.debug.Lib_SourceCodeHelper;
 
 /**
  * Created by zhusx on 2015/8/5.
@@ -13,5 +16,22 @@ public class _BaseActivity extends Lib_BaseActivity {
      */
     public void onClickReturn(View v) {
         finish();
+    }
+
+    Lib_SourceCodeHelper helper;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (helper == null) {
+            helper = new Lib_SourceCodeHelper(getClass());
+            helper._onCreateOptionsMenu(this, menu);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        helper._onOptionsItemSelected(this, item);
+        return super.onOptionsItemSelected(item);
     }
 }
