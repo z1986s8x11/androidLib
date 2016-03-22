d195c60250733a6d63ca95157e1b0d01d7e66915
z1986s8x11 <z1986s8x11@163.com>
2016-03-22 11:07:50 +0800

    init

--- app/src/main/java/com/zsx/debug/Lib_SourceCodeHelper.java
+++ app/src/main/java/com/zsx/debug/Lib_SourceCodeHelper.java
@@ -2,11 +2,16 @@ package com.zsx.debug;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.zsx.R;
 import com.zsx.app.Lib_BaseFragmentActivity;
 import com.zsx.app._PublicFragmentActivity;
+import com.zsx.widget.slidingmenu.SlidingMenu;
 
 /**
  * Author       zhusx
@@ -39,6 +44,23 @@ public class Lib_SourceCodeHelper {
     public void _onCreateOptionsMenu(Context context, Menu menu) {
         if (cls != null) {
             menu.add(1, 1986, 0, cls.getSimpleName());
+        }
+    }
+
+    SlidingMenu mSlidingMenu;
+
+    public void initContextView(Lib_BaseFragmentActivity activity) {
+        if (mSlidingMenu == null) {
+            mSlidingMenu = new SlidingMenu(activity, SlidingMenu.SLIDING_CONTENT);
+            final View right = LayoutInflater.from(activity).inflate(R.layout.lib_layout_linearlayout, null, false);
+            mSlidingMenu.setMenu(right);
+            mSlidingMenu.setBehindWidth(activity._getFullScreenWidth() - 200);
+            final String fileName = "java/" + activity.getClass().getName().replace(".", "/") + ".java";
+            P_SourceCodeFragment fragment = new P_SourceCodeFragment();
+            Bundle b = new Bundle();
+            b.putString(Lib_BaseFragmentActivity._EXTRA_String, fileName);
+            fragment.setArguments(b);
+            activity.getSupportFragmentManager().beginTransaction().replace(R.id.lib_content, fragment).commitAllowingStateLoss();
         }
     }
 }
--- app/src/main/java/com/zsx/debug/P_SourceCodeFragment.java
+++ app/src/main/java/com/zsx/debug/P_SourceCodeFragment.java
@@ -123,19 +123,6 @@ public class P_SourceCodeFragment extends Lib_BaseFragment {
         }
     }
 
-//    public static void initContextView(Lib_BaseFragmentActivity activity) {
-//        SlidingMenu mSlidingMenu = new SlidingMenu(activity, SlidingMenu.SLIDING_CONTENT);
-//        final View right = LayoutInflater.from(activity).inflate(R.layout.lib_layout_linearlayout, null, false);
-//        mSlidingMenu.setMenu(right);
-//        mSlidingMenu.setBehindWidth(activity._getFullScreenWidth() - 200);
-//        final String fileName = "java/" + activity.getClass().getName().replace(".", "/") + ".java";
-//        Lib_SourceCodeFragment fragment = new Lib_SourceCodeFragment();
-//        Bundle b = new Bundle();
-//        b.putString(_EXTRA_String, fileName);
-//        fragment.setArguments(b);
-//        activity.getSupportFragmentManager().beginTransaction().replace(R.id.lib_content, fragment).commitAllowingStateLoss();
-//    }
-
     public String assetsName = "source.zip";
 
     private void initData(final String fileName) {
