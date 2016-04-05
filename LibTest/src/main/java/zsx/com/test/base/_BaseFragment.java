f2686c19ef1775e89fd322521016b703896fdb55
z1986s8x11 <z1986s8x11@163.com>
2016-04-05 15:50:46 +0800

    init

---
+++ LibTest/src/main/java/zsx/com/test/base/_BaseFragment.java
@@ -0,0 +1,11 @@
+package zsx.com.test.base;
+
+import com.zsx.app.Lib_BaseFragment;
+
+/**
+ * Author       zhusx
+ * Email        327270607@qq.com
+ * Created      2016/4/5 14:29
+ */
+public class _BaseFragment extends Lib_BaseFragment {
+}
