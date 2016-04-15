722ca41ee567e493db9f1fd00f93801f6cfa18e5
z1986s8x11 <z1986s8x11@163.com>
2016-04-15 13:49:59 +0800

    init

--- help/src/main/java/org/zsx/android/api/design/CollapsingToolbarLayout_Activity.java
+++ help/src/main/java/org/zsx/android/api/design/CollapsingToolbarLayout_Activity.java
@@ -6,19 +6,24 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import org.zsx.android.api.R;
 import org.zsx.android.base._BaseActivity;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Author       zhusx
  * Email        327270607@qq.com
  * Created      2016/4/13 15:23
  */
 public class CollapsingToolbarLayout_Activity extends _BaseActivity {
-    CollapsingToolbarLayout collapsingToolbarLayout;
+    @InjectView(R.id.collapsing_toolbar)
+    public CollapsingToolbarLayout collapsingToolbarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.widget_design_collapsingtollbar);
-        //CollapsingToolbarLayout.setTitle("");当CollapsingToolbarLayout全屏没有折叠时，title显示的是大字体，在折叠的过程中，title不断变小到一定大小的效果。你可以调用setTitle(CharSequence)方法设置title
+        ButterKnife.inject(this);
+        collapsingToolbarLayout.setTitle("Title");//当CollapsingToolbarLayout全屏没有折叠时，title显示的是大字体，在折叠的过程中，title不断变小到一定大小的效果。你可以调用setTitle(CharSequence)方法设置title
         //CollapsingToolbarLayout.setStatusBarScrim();调用方法setStatusBarScrim(Drawable)。还没研究明白，不过这个只能在Android5.0以上系统有效果。
     }
 }
