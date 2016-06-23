ee4a2e516083a61928fc0fad04123cd4706396c7
z1986s8x11 <z1986s8x11@163.com>
2016-06-23 14:41:46 +0800

    init

--- app/src/main/java/com/zsx/widget/view/Lib_Widget_ViewPager.java
+++ app/src/main/java/com/zsx/widget/view/Lib_Widget_ViewPager.java
@@ -2,6 +2,10 @@ package com.zsx.widget.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -24,6 +28,8 @@ import com.zsx.itf.Lib_OnCycleListener;
 public class Lib_Widget_ViewPager extends ViewPager {
     private boolean isScrollable = true;//是否可以滑动
     private boolean isAutoScroll;//是否自动滚动
+    private Bitmap mBigBitmap;
+    private Paint b;
 
     private AutoScroll autoScroll;
 
@@ -164,5 +170,41 @@ public class Lib_Widget_ViewPager extends ViewPager {
                 pHandler.postDelayed(runnable, interval);
             }
         }
+    }
+
+    /**
+     * 设置大图的背景
+     */
+    public void _setBigBackground(Bitmap bigBitmap) {
+        this.mBigBitmap = bigBitmap;
+        this.b = new Paint(Paint.ANTI_ALIAS_FLAG);
+        this.b.setFilterBitmap(true);
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        if (this.mBigBitmap != null) {
+            PagerAdapter adapter = this.getAdapter();
+            if (adapter != null) {
+                int count = adapter.getCount();
+                int width = this.mBigBitmap.getWidth();
+                int height = this.mBigBitmap.getHeight();
+                if (count > 0) {
+                    int x = getScrollX();
+                    //子View中背景图片需要显示的宽度，放大背景图或缩小背景图。
+                    int n = height * this.getWidth() / this.getHeight();
+                    //(width - n) / (count - 1)表示除去显示第一个ViewPager页面用去的背景宽度，剩余的ViewPager需要显示的背景图片的宽度。
+                    //getWidth()等于ViewPager一个页面的宽度，即手机屏幕宽度。在该计算中可以理解为滑动一个ViewPager页面需要滑动的像素值。
+                    //((width - n) / (count - 1)) / getWidth()也就表示ViewPager滑动一个像素时，背景图片滑动的宽度。
+                    //x * ((width - n) / (count - 1)) / getWidth()也就表示ViewPager滑动x个像素时，背景图片滑动的宽度。
+                    //背景图片滑动的宽度的宽度可以理解为背景图片滑动到达的位置。
+                    int w = (x + this.getWidth()) * ((width - n) / count) / this.getWidth();
+                    canvas.drawBitmap(this.mBigBitmap, new Rect(w, 0, n + w, height), new Rect(x, 0, x + this.getWidth(), this.getHeight()), this.b);
+                } else {
+                    canvas.drawBitmap(this.mBigBitmap, new Rect(0, 0, width, height), new Rect(0, 0, this.getWidth(), this.getHeight()), this.b);
+                }
+            }
+        }
+        super.dispatchDraw(canvas);
     }
 }
